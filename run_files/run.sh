b5e6e18056f118e3b2840e6b432d8e6c5001825e
olga.fourkioti <olgafour1@gmail.com>
2022-01-18 16:12:45 +0000

    alma

--- run_files/run.sh
+++ run_files/run.sh
@@ -5,8 +5,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=48:05:00
-#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/transformer_k_3.out
-#SBATCH --error=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/transformer_k_3.err
+#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/distance_log.out
+#SBATCH --error=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/distance_log.err
 #SBATCH --partition=gpu
 
 
@@ -14,5 +14,5 @@ module load anaconda/3
 source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 conda activate alma
 cd /home/ofourkioti/Projects/toy_dataset_MIL/
-python run.py --experiment_name transformer_k_3  --mode euclidean --k 3  --input_shape 27 27 3 --extention bmp --data colon  --weight_file
+python run.py --experiment_name distance_log --mode euclidean --k 5  --input_shape 27 27 3 --extention bmp --data colon  --weight_file --distance log
 

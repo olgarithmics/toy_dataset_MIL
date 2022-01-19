38fcbccb938e835fe4491ae80d1cedf899b4197f
olga.fourkioti <olgafour1@gmail.com>
2022-01-19 11:17:51 +0000

    run.sh

--- run_files/run.sh
+++ run_files/run.sh
@@ -5,8 +5,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=48:05:00
-#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/distance_euclidean.out
-#SBATCH --error=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/distance_euclidean.err
+#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/distance_exp_dk.out
+#SBATCH --error=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/distance_exp_dk.err
 #SBATCH --partition=gpu
 
 
@@ -14,5 +14,5 @@ module load anaconda/3
 source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 conda activate alma
 cd /home/ofourkioti/Projects/toy_dataset_MIL/
-python run.py --experiment_name distance_euclidean --mode vaegan --k 5  --input_shape 27 27 3 --extention bmp --data colon  --weight_file --distance euclidean
+python run.py --experiment_name distance_exp_dk --mode vaegan --k 5  --input_shape 27 27 3 --extention bmp --data colon  --weight_file --distance exp
 

397b00292789ff90c8b52aefb4f75add6b58848a
olga.fourkioti <olgafour1@gmail.com>
2022-01-20 16:53:16 +0000

    alma

--- run_files/run.sh
+++ run_files/run.sh
@@ -5,8 +5,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=48:05:00
-#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/dicriminator_0.8.out
-#SBATCH --error=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/dicriminator_0.8.err
+#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/value_0.7.out
+#SBATCH --error=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/ColonCancer_results/value_0.7.err
 #SBATCH --partition=gpu
 
 
@@ -14,5 +14,5 @@ module load anaconda/3
 source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 conda activate alma
 cd /home/ofourkioti/Projects/toy_dataset_MIL/
-python run.py --experiment_name dicriminator_0.8 --mode vaegan --k 5  --input_shape 27 27 3 --extention bmp --data colon  --weight_file --prob 0.8
+python run.py --experiment_name value_0.7 --mode vaegan --k 5  --input_shape 27 27 3 --extention bmp --data colon  --weight_file --prob 0.7
 

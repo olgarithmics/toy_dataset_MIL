12f95914e28c9021c31010ee5cacb7ba68d233f9
olga.fourkioti <olgafour1@gmail.com>
2022-01-24 13:56:06 +0000

    alma

--- training/training.py
+++ training/training.py
@@ -194,8 +194,8 @@ class GraphAttnet:
         # alpha = NeighborAggregator(output_dim=1, name="alpha")([neigh, self.inputs["adjacency_matrix"]])
         #
         # attention_output = multiply([alpha, self.outputs["bag"]], name="mul")
-        attention_weights = NeighborAttention(embed_dim=128)([self.outputs["bag"], self.inputs["adjacency_matrix"]])
-        attention_output = multiply([attention_weights, self.outputs["bag"]], name="mul")
+        attention_output, attention_weights = NeighborAttention(embed_dim=256)([self.outputs["bag"], self.inputs["adjacency_matrix"]])
+
 
         #attention_output=TransformerBlock(embed_dim=256, ff_dim=256, training=self.training)([self.outputs["bag"], self.inputs["adjacency_matrix"]])
 

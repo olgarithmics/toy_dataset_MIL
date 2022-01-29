2f49e063a92aa099fc092e3a4d07030fee8de4cb
olga.fourkioti <olgafour1@gmail.com>
2022-01-29 16:50:00 +0000

    run

--- run_files/run.sh
+++ run_files/run.sh
@@ -5,12 +5,12 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=48:05:00
-#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets_util/ColonCancer_results/tan.out
-#SBATCH --error=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets_util/ColonCancer_results/tan.err
+#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets_util/ColonCancer_results/exp.out
+#SBATCH --error=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets_util/ColonCancer_results/exp.err
 #SBATCH --partition=gpu
 
 module load anaconda/3
 source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 conda activate alma
 cd /home/ofourkioti/Projects/toy_dataset_MIL/
-python run.py --experiment_name tan --mode euclidean --k 3  --input_shape 27 27 3 --extention bmp --data colon  --prob 0.75
+python run.py --experiment_name exp --mode euclidean --k 3  --input_shape 27 27 3 --extention bmp --data colon  --prob 0.75 --weight_file

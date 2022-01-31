c1d02215833c1728609f85047f2cb07175a91324
olga.fourkioti <olgafour1@gmail.com>
2022-01-31 16:13:15 +0000

    run sh

--- run_files/run.sh
+++ run_files/run.sh
@@ -5,12 +5,12 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=48:05:00
-#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets_util/ColonCancer_results/prob_7.out
-#SBATCH --error=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets_util/ColonCancer_results/prob_7.err
+#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets_util/ColonCancer_results/prob_8.out
+#SBATCH --error=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets_util/ColonCancer_results/prob_8.err
 #SBATCH --partition=gpu
 
 module load anaconda/3
 source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 conda activate alma
 cd /home/ofourkioti/Projects/toy_dataset_MIL/
-python run.py --experiment_name prob_7 --mode vaegan --k 3  --input_shape 27 27 3 --extention bmp --data colon  --prob 0.75 --weight_file --sigma 0.5 --prob 0.7
+python run.py --experiment_name prob_8 --mode vaegan --k 3  --input_shape 27 27 3 --extention bmp --data colon  --prob 0.75 --weight_file --sigma 0.5 --prob 0.8

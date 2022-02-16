9ce01415ea1949932ff8e6ccc10f945c03b11e73
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2022-02-16 20:19:32 +0000

    alma@

--- run_files/run.sh
+++ run_files/run.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=70:05:00
-#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets_util/ColonCancer_results/info_gan_euclidean.out
+#SBATCH --output=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets_util/ColonCancer_results/info_gan_euclidean_075.out
 #SBATCH --error=/data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets_util/ColonCancer_results/info_gan_layer.err
 #SBATCH --partition=gpu
 
@@ -13,4 +13,4 @@ module load anaconda/3
 source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 conda activate alma
 cd /home/ofourkioti/Projects/toy_dataset_MIL/
-python run.py --experiment_name info_gan_euclidean --mode vaegan  --k 3 --input_shape 27 27 3 --extention bmp --data colon  --data_path ColonCancer  --vaegan_save_dir /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/infogan_weights --dist euclidean --prob 0
+python run.py --experiment_name info_gan_euclidean_075 --mode vaegan  --k 3 --input_shape 27 27 3 --extention bmp --data colon  --data_path ColonCancer  --vaegan_save_dir /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/infogan_weights --dist euclidean --prob 0.75

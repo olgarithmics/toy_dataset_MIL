3a4cda6a9a016d5711efb501a2e7ffaff296d341
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2022-02-16 19:42:51 +0000

    alma@

--- run_files/run.sh
+++ run_files/run.sh
@@ -13,4 +13,4 @@ module load anaconda/3
 source /opt/software/applications/anaconda/3/etc/profile.d/conda.sh
 conda activate alma
 cd /home/ofourkioti/Projects/toy_dataset_MIL/
-python run.py --experiment_name info_gan --mode vaegan  --k 3 --input_shape 27 27 3 --extention bmp --data colon  --data_path ColonCancer  --vaegan_save_dir /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/infogan_weights --dist euclidean --prob 0
+python run.py --experiment_name info_gan_euclidean --mode vaegan  --k 3 --input_shape 27 27 3 --extention bmp --data colon  --data_path ColonCancer  --vaegan_save_dir /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/toy_datasets/infogan_weights --dist euclidean --prob 0
